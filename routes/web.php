84c90ff916b331810418655d4893e4fa6191b510
Jordon Baade <github@jordonbaade.com>
2022-12-01 22:17:28 -0500

    Refactoring for future ease

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,24 @@ Route::get('/', function () {
            )->explode(PHP_EOL.PHP_EOL)->map(
                fn($i) => str($i)->explode(PHP_EOL)->map(fn($i)=>(int)$i)
            )->map->sum()->sort()->tap(function($caloriesByElf) {
-               dump('Day 1, Part 1: '.$caloriesByElf->last().' Calories');
-               dump('Day 1, Part 2: '.$caloriesByElf->splice(-3)->sum().' Calories');
+               dd(
+                   'Day 1, Part 1: '.$caloriesByElf->last().' Calories',
+                   'Day 1, Part 2: '.$caloriesByElf->splice(-3)->sum().' Calories'
+               );
         }),
         // Day 2
         fn() => str(
                File::get(resource_path('aoc_input/day1/input.txt'))
-           )->tap(function($caloriesByElf) {
-               dump('Day 2, Part 1: ');
-               dump('Day 2, Part 2: ');
-        }),
+           )->dd('dostuffhere')->tap(function($caloriesByElf) {
+               dd(
+                   'Day 2, Part 1: ',
+               'Day 2, Part 2: '
+               );
+           }),
         // Day 2
 //        fn() => str(
 //            File::get(resource_path('aoc_input/day1/input.txt'))
-//        )->tap(function($caloriesByElf) {
+//        )->dd('dostuffhere')->tap(function($caloriesByElf) {
 //            dump('Day 2, Part 1: ');
 //            dump('Day 2, Part 2: ');
 //        }),
